0ff4410d956eaa341e72e29878e56383473255c6
Michael Vilabrera <m.vilabrera@gmail.com>
2018-02-19 17:45:01 -0500

    initial commit

---
+++ AWSiOS/AWSiOS/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  AWSiOS
+//
+//  Created by Michael Vilabrera on 2/19/18.
+//  Copyright © 2018 Michael Vilabrera. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
